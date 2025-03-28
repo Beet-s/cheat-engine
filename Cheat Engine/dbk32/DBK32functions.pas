1d85115cfc34ed6f2d914fb80254e9407e1c5232
Beet-s <natefrost18@gmail.com>
2025-03-28 16:22:52 -0400

    Update DBK32functions.pas
    changed servicename on line 3194, DBK64.sys on line 3201 and DBK32.sys on line 3206

--- Cheat Engine/dbk32/DBK32functions.pas
+++ Cheat Engine/dbk32/DBK32functions.pas
@@ -3191,19 +3191,19 @@ begin
         if not fileexists(dataloc) then
         begin
 
-          servicename:='CEDRIVER73';
+          servicename:='justbeetingaround64';
           ultimapservicename:='ULTIMAP2';
           processeventname:='DBKProcList60';
           threadeventname:='DBKThreadList60';
 
           if iswow64 then
           begin
-            sysfile:='dbk64.sys';
+            sysfile:='justbeetingaround64.sys';
             ultimapsysfile:='ultimap2-64.sys';
           end
           else
           begin
-            sysfile:='dbk32.sys';
+            sysfile:='justbeetingaround32.sys';
             ultimapsysfile:='';
           end;
 
